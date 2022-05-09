46a87094c76a577a5e2adf2fb9900a8ba7a76a10
rifatrakib <Rakib.1508@outlook.com>
2022-05-09 06:16:36 +0600

    modified process runner script

--- main.py
+++ main.py
@@ -1,26 +1,18 @@
 import subprocess
 
 
-def invoke_spider(spider_name, **kwargs):
+def invoke_spider(spider_name):
     full_command = f'scrapy crawl {spider_name} '
-    for key, value in kwargs.items():
-        full_command = full_command + f'-{key} {value} '
-    
     print(full_command)
     subprocess.run(full_command, shell=True)
 
 
 if __name__ == '__main__':
     spider_parameters = {
-        'property_spider': {
-            'output': {'O': 'data/property-sales-data.jl'},
-            'crawl': False,
-        },
-        'automobile_spider': {
-            'output': {'O': 'data/automobile-repair-dealer-data.jl'},
-            'crawl': False,
-        }
+        'property_spider': False,
+        'automobile_spider': False,
+        'business_spider': False,
     }
-    for spider, parameters in spider_parameters.items():
-        if parameters['crawl']:
-            invoke_spider(spider, **parameters['output'])
+    for spider, crawl in spider_parameters.items():
+        if crawl:
+            invoke_spider(spider)
