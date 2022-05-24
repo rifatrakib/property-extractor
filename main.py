136e559d1e29b611e0e37cf1b66c4e8d6b6e9157
rifatrakib <Rakib.1508@outlook.com>
2022-05-24 19:52:02 +0600

    file segmentation logic implemented using pandas

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import subprocess
+import pandas as pd
 
 
 def invoke_spider(spider_name):
@@ -7,12 +8,24 @@ def invoke_spider(spider_name):
     subprocess.run(full_command, shell=True)
 
 
+def file_segmentation(spider):
+    identifier = spider.replace('spider', '').replace('_', '')
+    df = pd.read_csv(f'data/csv/{identifier}-data.csv', dtype='object')
+    row_count = df.shape[0]
+    for cut_off in range(0, row_count, 10000):
+        segment = df.iloc[cut_off:cut_off+10000, :]
+        segment.to_csv(f'segmented-data/csv/{identifier}/{identifier}-data-{cut_off}-{cut_off+10000}.csv', sep='|')
+        segment.to_json(f'segmented-data/json/{identifier}/{identifier}-data-{cut_off}-{cut_off+10000}.json', orient='records')
+        segment.to_json(f'segmented-data/jsonline/{identifier}/{identifier}-data-{cut_off}-{cut_off+10000}.jl', orient='records', lines=True)
+
+
 if __name__ == '__main__':
     spider_parameters = {
-        'property_spider': True,
-        'automobile_spider': True,
-        'business_spider': True,
+        'property_spider': False,
+        'automobile_spider': False,
+        'business_spider': False,
     }
     for spider, crawl in spider_parameters.items():
         if crawl:
             invoke_spider(spider)
+        file_segmentation(spider)
